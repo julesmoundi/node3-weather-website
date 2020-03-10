d381d6f62d07901b6399297274f3242634a82250
EYANGO MOUNDI Jules Junior <julesmoundi@gmail.com>
2020-03-10 22:33:29 +0100

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,7 @@ weatherForm.addEventListener('submit', e => {
   prevision.textContent = '';
 
 
-  fetch('http://localhost:3000/weather?address=' + location).then(response => {
+  fetch('/weather?address=' + location).then(response => {
     response.json().then(data => {
       if (data.error) {
         return titre.textContent = data.error;
